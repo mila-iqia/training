76d2051e556bb77fa5db1674342d4cb367375cf6
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-04-24 14:32:08 -0400

    update the dependencies install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,9 +4,6 @@ SCRIPT_PATH=$(dirname "$0")
 source ${SCRIPT_PATH}/config.env
 
 if [ ! -f dependencies.cache ]; then
-
-    sudo apt-get install -y $(cat apt-packages)
-
     # Install miniconda
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $TEMP_DICRETORY
 
@@ -17,15 +14,13 @@ if [ ! -f dependencies.cache ]; then
     conda create -n mlperf python=3.6
     source activate mlperf
 
-    # Install the perf library
-    cd common
-    python setup.py install
-
-    echo "Install pytorch and tensorflow now"
-
-    pip install Cython
-    pip install --no-deps -r requirements.txt
-   
     echo 'DONE'
     touch dependencies.cache
 fi
+
+sudo apt-get install -y $(cat apt-packages)
+
+# Install the perf library
+pip install -e common
+pip install Cython
+pip install --no-deps -r requirements.txt
