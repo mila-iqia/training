3ffe60ac2e63133431349b741612327c24cd7bff
Olivier Breuleux <breuleux@gmail.com>
2019-11-29 10:34:28 -0500

    Add install_python_dependencies

--- install_python_dependencies.sh
+++ install_python_dependencies.sh
@@ -2,7 +2,8 @@
 
 set -ex
 
-echo 'Install required Python dependencies'
-conda activate mlperf
+echo 'Install Python packages'
+pip install Cython==0.29.14
+pip install numpy==1.17.4
 pip install --no-deps -r requirements.txt
-echo 'DONE: Install required Python dependencies'
+echo 'DONE: Install Python packages'
