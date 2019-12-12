c48adfcd82e19f094a7fff640481f1ec87133b16
Olivier Breuleux <breuleux@gmail.com>
2019-12-12 15:28:59 -0500

    Rewrite report script

--- milabench/milabench/report/report.py
+++ milabench/milabench/report/report.py
@@ -1,272 +1,376 @@
+from hrepr import HTML
+from collections import defaultdict
 import json
 import os
 import numpy as np
 import glob
 import pandas as pd
+from pandas import DataFrame, Series
+import math
+from itertools import chain
+
+
+H = HTML()
+
+
+_exclude = {
+    'convnet_distributed',
+    'convnet_distributed_fp16',
+    'dcgan_all',
+}
+
+
+def load_comparison_data2(gpu_model):
+    import os
+    from milabench import report
+    dirname = os.path.dirname(report.__file__)
+    return pd.read_csv(f'{dirname}/target_{gpu_model}.csv',
+                       index_col='bench')
 
 
 def read_results(report_name):
     str_report = open(report_name, 'r').read().strip()[:-1]
-    return json.loads(str_report + ']')
-
-
-def printf(*args, indent=0, **kwargs):
-    print(' ' * (indent * 4), *args, **kwargs)
-
-
-class Accumulator:
-    def __init__(self):
-        self.values = []
-
-    def append(self, v):
-        self.values.append(v)
-
-    def max(self):
-        return max(self.values)
-
-    def min(self):
-        return min(self.values)
-
-    def sum(self):
-        return sum(self.values)
-
-    def __len__(self):
-        return len(self.values)
-
-    def avg(self):
-        return self.sum() / len(self)
-
-    def __repr__(self):
-        return f'(avg={self.avg}, {len(self)})'
+    if not str_report.endswith(']'):
+        str_report += ']'
+    return json.loads(str_report)
 
 
 def extract_reports(report_folder, report_names='baselines'):
-    perf_reports = {}
-    batch_reports = {}
+    filenames = chain(
+        glob.glob(f'{report_folder}/**/{report_names}_*.json'),
+        glob.glob(f'{report_folder}/{report_names}_*.json')
+    )
+    reports = defaultdict(list)
+    for filename in filenames:
+        data = read_results(filename)
+        for entry in data:
+            if entry['name'] in _exclude:
+                continue
+            entry['__path__'] = filename
+            reports[entry['name']].append(entry)
 
-    for vendor_name in os.listdir(report_folder):
-        printf(f'Processing vendor: {vendor_name}')
-
-        baselines_results = glob.glob(f'{report_folder}/{vendor_name}/{report_names}*')
-
-        device_count = len(baselines_results)
-        printf(f'Found reports for {device_count} GPUs', indent=1)
-        vendor_perf_reports = {}
-        vendor_batch_loss_reports = {}
-
-        perf_reports[vendor_name] = vendor_perf_reports
-        batch_reports[vendor_name] = vendor_batch_loss_reports
-
-        # we want device 0 to be first since it is the report with the most reports (distributed)
-        baselines_results.sort()
-
-        for idx, device_reports in enumerate(baselines_results):
-            printf(f'Reading (device_id: {idx}) (report: {device_reports})', indent=2)
-
-            # List of Results for each Trial
-            reports = read_results(device_reports)
-
-            for bench_result in reports:
-                bench_name = bench_result['name']
-                uid = bench_result['unique_id']
-                version = bench_result['version']
-                unique_id = bench_name  # (uid, bench_name)
-
-                # Select the task that matters
-                if bench_name == 'wlm' and bench_result['model'] != 'GRU':
-                    continue
-
-                if bench_name == 'wlmfp16' and bench_result['model'] != 'GRU':
-                    continue
-
-                if bench_name == 'loader' and bench_result['batch_size'] != 256:
-                    continue
-
-                if bench_name == 'toy_lstm' and bench_result['dtype'] != 'float32':
-                    continue
-
-                if bench_name == 'ssd' and len(bench_result['vcd']) > 1:
-                    continue
-
-                if bench_name == 'image_loading_loader_pytorch_loaders.py':
-                    batch_size = bench_result['batch_size']
-                    unique_id = f'{unique_id}_{batch_size}'
-                    version = f'{unique_id}_{batch_size}'
-
-                printf(f'Processing {bench_name} {version}', indent=3)
-
-                if unique_id in vendor_perf_reports and idx == 0:
-                    printf(f'[!] Error two benchmark with the same name (name: {bench_name})', indent=4)
-                elif idx == 0:
-                    perf_report = dict(
-                        train_item=Accumulator(),
-                        unique_id=uid,
-                        version=version,
-                        error=[],
-                        name=bench_name,
-                    )
-                    vendor_perf_reports[unique_id] = perf_report
-
-                elif unique_id not in vendor_perf_reports:
-                    printf(f'[!] Error missing benchmark for previous GPU (name: {bench_name})', indent=4)
-                    perf_report = dict(train_item=Accumulator(), unique_id=uid, version=version, error=[])
-                    vendor_perf_reports[unique_id] = perf_report
-
-                # Accumulate values
-                perf_report = vendor_perf_reports[unique_id]
-                if perf_report['unique_id'] != uid:
-                    printf(f'[!] Error unique_ids do not match cannot aggregate (name: {bench_name})!', indent=4)
-                    perf_report['error'].append('id mismatch')
-
-                elif perf_report['version'] != version:
-                    printf(f'[!] Error versions do not match cannot aggregate!', indent=4)
-                    perf_report['error'].append('version mismatch')
-
-                else:
-                    perf_report['train_item'].append(bench_result['train_item']['avg'])
-
-                batch_loss = bench_result.get('batch_loss')
-                if batch_loss is None:
-                    printf(f'/!\\ No batch loss for benchmark (name: {bench_name})', indent=4)
-                else:
-                    if unique_id not in vendor_batch_loss_reports:
-                        vendor_batch_loss_reports[unique_id] = []
-
-                    vendor_batch_loss_reports[unique_id].append(batch_loss)
-
-    return perf_reports
+    return reports
 
 
-def filer_report(rep, agg):
-    new_rep = {}
-
-    for vendor, report in rep.items():
-        vendor_rep = {}
-        new_rep[vendor] = vendor_rep
-
-        for name, v in report.items():
-            key = f'{name}_{v["version"]}_{v["unique_id"]}'
-            vendor_rep[key] = getattr(v['train_item'], agg)()
-
-    return new_rep
+def _report_nans(reports):
+    nans = []
+    for name, entries in reports.items():
+        for entry in entries:
+            if any(math.isnan(x) for x in entry['batch_loss']):
+                nans.append(entry)
+    return nans
 
 
-def load_comparison_data():
-    import os
-    from milabench import report
-    dirname = os.path.dirname(report.__file__)
-    return pd.read_csv(f'{dirname}/data.csv', index_col='bench')
+def _report_pergpu(baselines, reports, measure='mean'):
+    results = defaultdict(lambda: defaultdict(list))
+    all_devices = set()
+    for name, entries in reports.items():
+        for entry in entries:
+            devices = [int(device_id) for device_id in entry['vcd'].split(',')]
+            if len(devices) == 1:
+                device, = devices
+                all_devices.add(device)
+                results[name][device].append(entry['train_item']['avg'])
+
+    all_devices = list(sorted(all_devices))
+
+    results = {
+        name: {device: getattr(Series(data), measure)()
+               for device, data in device_results.items()}
+        for name, device_results in results.items()
+    }
+
+    df = DataFrame(results).transpose()
+    df = df.reindex(columns=all_devices)
+
+    maxes = df.loc[:, all_devices].max(axis=1).transpose()
+    df = (df.transpose() / maxes).transpose()
+
+    return df
 
 
-def show_perf(folder_name, report_name):
-    perf_reports = extract_reports(folder_name, report_name)
-    df = pd.DataFrame(filer_report(perf_reports, 'avg'))
+_table_style = H.style("""
+body {
+    font-family: monospace;
+}
+td, th {
+    text-align: right;
+    min-width: 75px;
+}
+.result-PASS {
+    color: green;
+    font-weight: bold;
+}
+.result-FAIL {
+    color: red;
+    font-weight: bold;
+}
+""")
 
-    sd = df.std(axis=1)
-    df.loc[:, 'result'] = (df.sum(axis=1) - df.max(axis=1) - df.min(axis=1)) / (df.count(axis=1) - 2)
-    df.loc[:, 'sd'] = sd
-    df.loc[:, 'sd%'] = sd / df.loc[:, 'result']
 
-    return df.loc[:, ('result', 'sd', 'sd%')]
+def _style(df):
+
+    def _redgreen(value):
+        return 'color: green' if value else 'color: red'
+
+    def _gpu_pct(value):
+        if value >= 0.9:
+            color = '#080'
+        elif value >= 0.8:
+            color = '#880'
+        elif value >= 0.7:
+            color = '#F80'
+        else:
+            color = '#F00'
+        return f'color: {color}'
+
+    def _perf(values):
+        return (values >= df['perf_tgt']).map(_redgreen)
+
+    def _sd(values):
+        return (values <= df['sd%_tgt']).map(_redgreen)
+
+    # Text formatting
+    sty = df.style
+    sty = sty.format(_formatters)
+
+    # Format GPU efficiency map columns
+    gpu_columns = set(range(16)) & set(df.columns)
+    sty = sty.applymap(_gpu_pct, subset=list(gpu_columns))
+
+    # Format perf column
+    if 'perf' in df.columns and 'perf_tgt' in df.columns:
+        sty = sty.apply(_perf, subset=['perf'])
+        sty = sty.applymap(
+            lambda x: 'font-weight: bold' if x >= 1 else '',
+            subset=['perf']
+        )
+
+    # Format sd% column
+    if 'sd%' in df.columns and 'sd%_tgt' in df.columns:
+        sty = sty.apply(_sd, subset=['sd%'])
+
+    # Format pass/fail column
+    for col in ['pass', 'sd%_pass', 'perf_pass']:
+        if col in df.columns:
+            sty = sty.applymap(_redgreen, subset=[col])
+
+    return sty
+
+
+def _display_title(file, title, stdout_display=True):
+    if stdout_display:
+        print()
+        print('=' * len(title))
+        print(title)
+        print('=' * len(title))
+        print()
+
+    if file:
+        print(H.h2(title), file=file)
+
+
+def _display_table(file, title, table, stdout_display=True):
+    _display_title(file, title, stdout_display=stdout_display)
+
+    if stdout_display:
+        print(table.to_string(formatters=_formatters))
+
+    if file:
+        sty = _style(table)
+        print(sty._repr_html_(), file=file)
+
+
+def _report_global(baselines, reports):
+    df = DataFrame(
+        {
+            name: {
+                'n': len(entries),
+                'mean': Series(x['train_item']['avg'] for x in entries).mean(),
+                'sd': Series(x['train_item']['avg'] for x in entries).std(),
+            }
+            for name, entries in reports.items()
+        }
+    ).transpose()
+    df['target'] = baselines['target']
+    df['perf_tgt'] = baselines['perf_target']
+    df['sd%_tgt'] = baselines['sd_target']
+    df['perf'] = df['mean'] / df['target']
+    df['sd%'] = df['sd'] / df['mean']
+    df['perf_pass'] = df['perf'] >= df['perf_tgt']
+    df['sd%_pass'] = df['sd%'] <= df['sd%_tgt']
+    df['pass'] = df['perf_pass'] & df['sd%_pass']
+    return df
+
+
+def passfail(x):
+    return 'PASS' if x else 'FAIL'
+
+
+_formatters = {
+    'n': '{:.0f}'.format,
+    'target': '{:10.2f}'.format,
+    'sd%_tgt': '{:10.0%}'.format,
+    'mean': '{:10.2f}'.format,
+    'sd': '{:10.2f}'.format,
+    'perf': '{:10.2f}'.format,
+    'sd%': '{:10.1%}'.format,
+    'pass': passfail,
+    'sd%_pass': passfail,
+    'perf_pass': passfail,
+    0: '{:.0%}'.format,
+    1: '{:.0%}'.format,
+    2: '{:.0%}'.format,
+    3: '{:.0%}'.format,
+    4: '{:.0%}'.format,
+    5: '{:.0%}'.format,
+    6: '{:.0%}'.format,
+    7: '{:.0%}'.format,
+    8: '{:.0%}'.format,
+    9: '{:.0%}'.format,
+    10: '{:.0%}'.format,
+    11: '{:.0%}'.format,
+    12: '{:.0%}'.format,
+    13: '{:.0%}'.format,
+    14: '{:.0%}'.format,
+    15: '{:.0%}'.format,
+}
+
+
+def _format_df(df):
+    return df.to_string(formatters=_formatters)
 
 
 def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--reports', type=str, help='folder containing all the reports reports/folder/name.json')
-    parser.add_argument('--name', type=str, default='fast', help='name of the report to load')
-    parser.add_argument('--show-comparison', action='store_true', default=False)
-    parser.add_argument('--gpu-model', choices=['V100', 'MI50', 'RTX'], default=None)
-    parser.add_argument('--csv', type=str, default='',
-                        help='file to save the results in as CSV')
+
+    parser.add_argument('--reports',
+                        type=str,
+                        help=('folder containing all the reports:'
+                              ' reports/folder/jobs.json'))
+    parser.add_argument('--jobs',
+                        type=str,
+                        default='fast',
+                        help='name of the report to load')
+    parser.add_argument('--gpu-model',
+                        choices=['V100', 'MI50', 'RTX'],
+                        help='GPU model to compare against')
+    parser.add_argument('--html', type=str, default='',
+                        help='file in which to save the report')
+    parser.add_argument('--title', type=str, default='',
+                        help='title for the report')
 
     args = parser.parse_args()
 
-    df = show_perf(args.reports, args.name)
-    print()
+    reports = extract_reports(args.reports, args.jobs)
 
-    pd.set_option('display.max_colwidth', 80)
-    pd.set_option('display.max_rows', 500)
-    pd.set_option('display.max_columns', 500)
-    pd.set_option('display.width', 1000)
+    # Nan check
+    nans = _report_nans(reports)
+    for nan in nans:
+        print(f'nan found in batch loss for test "{name}"',
+              f'in file {entry["__path__"]}')
 
-    na_rows =  df[df.isna().any(axis=1)]
+    baselines = load_comparison_data2(args.gpu_model)
 
-    if args.show_comparison:
-        baselines = load_comparison_data()
+    outd = True
+    title = args.title or args.html.replace('.html', '')
 
-        if args.gpu_model:
-            baselines = baselines[args.gpu_model]
+    html = args.html and open(args.html, 'w')
+    print(f'<html><head><title>{title}</title></head><body>', file=html)
+    print(_table_style, file=html)
 
-        print('=' * 80)
-        print(' ' * 12, 'Baselines')
-        print('-' * 80)
-        print(baselines)
-        print('=' * 80, '\n')
+    _display_title(
+        title=f'{title} ({args.reports})',
+        file=html,
+        stdout_display=outd
+    )
 
-    if len(na_rows):
-        print('>' * 80)
-        print('Dropped Rows because of NaN\n')
-        print('/!\\ if there are nans; it means there were not enough observations to compute the standard deviation\n')
-        print(na_rows)
-        print('<' * 80)
+    df_global = _report_global(baselines, reports)
+    df = df_global.reindex(
+        columns=(
+            'n',
+            'target',
+            'perf_tgt',
+            'sd%_tgt',
+            'mean',
+            # 'sd',
+            'perf',
+            'perf_pass',
+            'sd%',
+            'sd%_pass',
+            # 'pass',
+        )
+    )
+    _display_table(
+        title=f'Results',
+        table=df,
+        file=html,
+        stdout_display=outd,
+    )
 
-    df = df.dropna()
-
-    if args.gpu_model:
-        baselines = load_comparison_data()[args.gpu_model]
-
-        names = []
-        for k in df.index:
-            name, _, _, = k.rsplit('_', maxsplit=2)
-            names.append(name)
-
-        df.insert(0, 'bench', names)
-        df = df.set_index('bench')
-
-        df['target'] = baselines
-        df['diff'] = (df['result'] - df['target']) / df['target']
-
-    print('--')
-    df = df.ix[:, ('target', 'result', 'sd', 'sd%', 'diff')]
-
-    if args.csv:
-        with open(args.csv, 'w') as f:
-            f.write(df.to_csv())
-
-    print(df)
-    print('--')
-
-    # Compute Scores
-    # --------------
-    perf_score = None
-    sd_quantile = df["sd%"].quantile(0.80) * 100
-    sd_sd = df["sd%"].std() * 100
+    _display_title(title='Global performance', file=html, stdout_display=outd)
 
     try:
-        perf_score = df['diff'].mean()
-        perf_dev = df['diff'].std()
+        df_perf = df.drop('scaling')
+    except KeyError:
+        df_perf = df
+    perf = (df_perf['perf'].prod()) ** (1/df_perf['perf'].count())
+    minreq = 0.9
+    success = perf > minreq
+    grade = (df_global['perf_pass'].sum()
+             + df_global['sd%_pass'].sum()
+             + success)
+    expected = 63
+    grade_success = grade == expected
 
-    except Exception as e:
-        print(f'/!\\ Warning exception {e} occurred')
+    if outd:
+        print(f'Mean performance (geometric mean): {perf:.2f}')
+        print(f'Minimum required:                  {minreq:.2f}')
+        print(f'Mean performance (pass or fail):   {passfail(success)}')
+        print(f'Grade:                             {grade}/{expected}')
+        print(f'Pass or fail (final):              {passfail(grade_success)}')
 
-    print()
-    print(f'Statistics               |     Value | Pass |')
-    print(f'-------------------------|-----------|------|')
-    print(f'Bench Passes             :           | {len(df["sd"]) == 19}')
-    print(f'Deviation Quantile (80%) : {sd_quantile:+.4f} % | {sd_quantile < 5} |')
+    if html:
+        tb = H.table(
+            H.tr(
+                H.th('Mean performance (geometric mean)'),
+                H.td(f'{perf:.2f}'),
+            ),
+            H.tr(
+                H.th('Minimum required'),
+                H.td(f'{minreq:.2f}'),
+            ),
+            H.tr(
+                H.th('Mean performance (pass or fail)'),
+                H.td[f'result-{passfail(success)}'](passfail(success)),
+            ),
+            H.tr(
+                H.th('Grade'),
+                H.td[f'result-{passfail(grade_success)}'](
+                    f'{grade}/{expected}'
+                ),
+            ),
+            H.tr(
+                H.th('Pass or fail (final)'),
+                H.td[f'result-{passfail(grade_success)}'](
+                    passfail(grade_success)
+                ),
+            ),
+        )
+        print(tb, file=html)
 
-    if False:
-        print(f'Deviation sd             : {      sd_sd:+.4f} % | {sd_sd < 5} |')
+    for measure in ['mean', 'min', 'max']:
+        df = _report_pergpu(baselines, reports, measure=measure)
+        _display_table(
+            title=f'Relative GPU performance ({measure})',
+            table=df,
+            file=html,
+            stdout_display=outd,
+        )
 
-    if perf_score:
-        print(f'Performance              : {perf_score:+.4f} % | {perf_score >= 0} ')
-
-        if False:
-            print(f'Performance sd           : {perf_dev:+.4f} % | {perf_score >= 0} ')
-
-    print('--')
+    html.write('</body></html>')
 
 
 if __name__ == '__main__':
