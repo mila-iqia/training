583903f5d38c330f37675d542e56721ba76d42ae
Olivier Breuleux <breuleux@gmail.com>
2019-12-12 18:54:57 -0500

    Fix run_10.sh

--- run_10.sh
+++ run_10.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-for i in {0..10}; do
+for i in {0..9}; do
     ./run.sh --uid ${i} "$@"
-
-    mkdir -p $BASE/output${i}
-    mv -f $BASE/output/* $BASE/output${i}
     sleep 10
 done
-
-
